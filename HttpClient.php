d732daaa8ce4dfe7aa04d78050241f714aa32419
lisy <768104762@qq.com>
2019-05-20 18:19:45 +0800

    GuzzleHttp的一个封装
    GuzzleHttp的一个封装

---
+++ HttpClient.php
@@ -0,0 +1,92 @@
+namespace app\common\library;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\TransferException;
+
+class HttpClient
+{
+    //错误信息
+    private $errorInfo = '';
+    //http请求类
+    public $httpClient = null;
+    //http状态码
+    private $httpCode = 200;
+    public function __construct()
+    {
+        $this->httpClient = new Client();
+    }
+    //单例
+    public static $_instance = NULL;
+    /**
+     * 静态工厂方法，返还此类的唯一实例
+     */
+    public static function getInstance()
+    {
+        if (is_null(self::$_instance))
+        {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+    /**
+     * 通用请求方法
+     * @param string $url        请求地址
+     * @param array $data        请求参数
+     * @param string $method     请求方法（GET/POST）
+     * @param bool $ajax         是否是ajax请求 true是，false不是
+     * @param array $headers     请求头
+     * @return bool|string(json/html)
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function httpClientFun($url='',$data=[],$method='POST',$ajax=false,$headers=[]){
+        try {
+            //构建请求参数
+            $param = [];
+            $param['verify'] = false;
+            if($ajax){
+                //判断是否为ajax请求
+                $param['json'] = $data;
+            }else{
+                //判断是否为post请求
+                $methodLower = strtolower($method);
+                if($methodLower=='post')
+                {
+                    $param['form_params'] = $data;
+                }
+            }
+            //如果请求头不为空，设置请求头
+            if(!empty($headers))
+            {
+                $param['headers'] = $headers;
+            }
+            //请求
+            $response = $this->httpClient->request($method,$url,$param);
+            //状态码
+            $resStatus = $response->getStatusCode();
+            if($resStatus!=200){
+                $this->httpCode = $resStatus;
+                $this->errorInfo = '请求失败';
+                return false;
+            }
+            $body = $response->getBody();
+            $contents = $body->getContents();
+            return $contents;
+        }catch(TransferException $exception){
+            $this->errorInfo = '错误的请求';
+            return false;
+        }
+    }
+    //输出错误信息
+    public function getErrorInfoStr(){
+        return $this->errorInfo;
+    }
+    //http的请求状态码
+    public function getHttpCode()
+    {
+        return $this->httpCode;
+    }
+    //返回 GuzzleHttp 原始的客户端
+    public function getHttpClient()
+    {
+        return $this->httpClient;
+    }
+}
